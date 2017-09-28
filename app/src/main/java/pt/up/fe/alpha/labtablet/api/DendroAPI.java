1a333a4f91967a3ee8855768d8a50e57b82feee9
Bruno Barros <brunocostabarros@hotmail.com>
2017-09-28 11:39:51 +0100

    Authenticate working

--- app/src/main/java/pt/up/fe/alpha/labtablet/api/DendroAPI.java
+++ app/src/main/java/pt/up/fe/alpha/labtablet/api/DendroAPI.java
@@ -5,16 +5,17 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.CookieManager;
+import java.net.HttpCookie;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
 
 import pt.up.fe.alpha.labtablet.async.AsyncUploader;
 import pt.up.fe.alpha.labtablet.models.Dendro.DendroConfiguration;
@@ -27,31 +28,54 @@ public class DendroAPI {
     public static String authenticate(Context context) throws Exception{
 
         DendroConfiguration conf = FileMgr.getDendroConf(context);
-        DefaultHttpClient httpclient = new DefaultHttpClient();
-        HttpParams httpParameters = new BasicHttpParams();
-        int timeout1 = 10000;
-        int timeout2 = 10000;
-        HttpConnectionParams.setConnectionTimeout(httpParameters, timeout1);
-        HttpConnectionParams.setSoTimeout(httpParameters, timeout2);
-        httpclient.setParams(httpParameters);
-        HttpPost httppost = new HttpPost(conf.getAddress() + "/login");
-        httppost.setHeader("Accept", "application/json");
-        httppost.setHeader("Content-Type", "application/json");
+
+
+        URL url = new URL(conf.getAddress() + "/login");
+
+        final String COOKIES_HEADER = "Set-Cookie";
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+        conn.setRequestProperty("Accept","application/json");
+        conn.setDoOutput(true);
 
         JSONObject json = new JSONObject();
         json.put("username", conf.getUsername());
         json.put("password", conf.getPassword());
 
-        StringEntity se = new StringEntity(json.toString());
-        httppost.setEntity(se);
+        OutputStream os = conn.getOutputStream();
+        os.write(json.toString().getBytes());
+        os.flush();
 
-        HttpResponse resp = httpclient.execute(httppost);
-        HttpEntity ent = resp.getEntity();
-        AsyncUploader.DendroResponse loginResponse = new Gson().fromJson(EntityUtils.toString(ent), AsyncUploader.DendroResponse.class);
+        BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+        String output;
+        StringBuilder response = new StringBuilder();
+        while ((output = br.readLine()) != null) {
+            response.append(output);
+            response.append('\r');
+        }
+        String mes = response.toString();
+        conn.disconnect();
+
+        AsyncUploader.DendroResponse loginResponse = new Gson().fromJson(mes, AsyncUploader.DendroResponse.class);
+
         if (loginResponse.result.equals(Utils.DENDRO_RESPONSE_ERROR)) {
             Log.e("AUTH", loginResponse.result + ": " + loginResponse.message);
             throw new Exception(loginResponse.result + ": " + loginResponse.message);
         }
-        return httpclient.getCookieStore().getCookies().get(0).getValue();
+
+        CookieManager msCookieManager = new CookieManager();
+        Map<String, List<String>> headerFields = conn.getHeaderFields();
+        List<String> cookiesHeader = headerFields.get(COOKIES_HEADER);
+
+        if (cookiesHeader != null) {
+            for (String cookie : cookiesHeader) {
+                msCookieManager.getCookieStore().add(null, HttpCookie.parse(cookie).get(0));
+            }
+        }
+
+        return msCookieManager.getCookieStore().getCookies().get(0).getValue();
     }
 }
